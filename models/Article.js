7e571a05bfdf50e1afe3d2dbf862c85c57e0a351
Anthony Mastanduno <anthonymastanduno23@gmail.com>
2019-05-14 12:24:15 -0400

    created article schema

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+let mongoose = require('mongoose');
+
+// save ref to Schema constructor
+let Schema = mongoose.Schema;
+// 
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        index: { unique: true },
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    // note is an object that stores the Note _id and ref links the ObjectId of Article to Note mode
+    // to populate the Article with associated note
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+let Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
